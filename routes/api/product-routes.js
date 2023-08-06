2b87d2e16b090742329b4ed8fa86b054ddd38222
restas13 <72560360+restas13@users.noreply.github.com>
2023-08-06 00:57:09 -0700

    fixxing the product route

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -108,71 +108,7 @@ router.get('/:id', (req, res) => {
         });
 });
 
-// Creates a new product using the request body
-router.post('/', (req, res) => {
-    Product.create(req.body)
-        .then((product) => {
 
-            if (req.body.tagIds && req.body.tagIds.length) {
-                const productTagIdArr = req.body.tagIds.map((tag_id) => {
-                    return {
-                        product_id: product.id,
-                        tag_id,
-                    };
-                });
-                return ProductTag.bulkCreate(productTagIdArr);
-            }
-
-            res.status(200).json(product);
-        })
-        .then((productTagIds) => res.status(200).json(productTagIds))
-        .catch((error) => {
-            console.log(error);
-            res.status(400).json(error);
-        });
-});
-
-// Used for updating a product
-router.put('/:id', (req, res) => {
-
-    Product.update(req.body, {
-        where: {
-            id: req.params.id,
-        },
-    })
-        .then((product) => {
-            if (req.body.tagIds && req.body.tagIds.length) {
-                const productTags = ProductTag.findAll({
-                    where: { product_id: req.params.id }
-                });
-                const productTagIds = productTags.map(({ tag_id }) => tag_id);
-
-                const newProductTags = req.body.tagIds
-                    .filter((tag_id) => !productTagIds.includes(tag_id))
-                    .map((tag_id) => {
-                        return {
-                            product_id: req.params.id,
-                            tag_id,
-                        };
-                    });
-
-                const productTagsToRemove = productTags
-                    .filter(({ tag_id }) => !req.body.tagIds.includes(tag_id))
-                    .map(({ id }) => id);
-
-
-                return Promise.all([
-                    ProductTag.destroy({ where: { id: productTagsToRemove } }),
-                    ProductTag.bulkCreate(newProductTags),
-                ]);
-            }
-
-            return res.json(product);
-        })
-        .catch((error) => {
-            res.status(400).json(error);
-        });
-});
 
 router.delete('/:id', (req, res) => {
     Product.destroy({
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,10 @@
-// importing the router and the api routes
 const router = require('express').Router();
 const apiRoutes = require('./api');
 
-// Using the api folder
 router.use('/api', apiRoutes);
 
-// Returning a message if the user ends up in the wrong route
 router.use((req, res) => {
     res.json({ message: "Uh oh, you're not supposed to be here" });
 })
 
-// exports the oruter
 module.exports = router;
